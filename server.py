5474d5b64542d25d7c05c69e5ec7867df70475b2
Leeyeon52 <dsy4907@gmail.com>
2025-05-27 12:44:18 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -1,35 +1,46 @@
 from flask import Flask, request, jsonify
 import joblib
-import pandas as pd  # 오타 수정
+import numpy as np
 
 app = Flask(__name__)
 
+DB_USER = "root"
+DB_PASSWORD = "1436"
+DB_HOST = "192.168.0.187"
+DB_PORT = "3306"
+DB_NAME = "test"
 # 학습된 모델 로드
-try:
-    model = joblib.load("lgbm_model.pkl")  # 저장된 모델 파일 로드
-    f_name = getattr(model, "feature_name_", None)  # feature_name_ 존재 여부 확인
-    if f_name:
-        print("Model Features:", f_name)
-except Exception as e:
-    print("Error loading model:", e)
-    model = None  # 모델 로드 실패 시 None 처리
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if model is None:
-        return jsonify({'error': 'Model not loaded'}), 500  # 모델이 없을 경우 예외 처리
+@app.route('/upload', methods=['POST'])
+def upload_csv():
+    if 'file' not in request.files:
+        return jsonify({"error": "파일이 없습니다!"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "파일 이름이 없습니다!"}), 400
     
     try:
-        data = request.get_json()  # JSON 데이터 받기
-        if not data:
-            return jsonify({'error': 'No input data provided'}), 400  # 데이터 없을 경우 예외 처리
-        
-        df = pd.DataFrame(data)  # JSON → DataFrame 변환
-        prediction = model.predict(df)  # 예측 실행
-        
-        return jsonify({'prediction': prediction.tolist()})  # 예측 결과 반환
+        df = pd.read_csv(file)
+        df.columns = [
+            'ID', '설립연도', '국가', '분야', '투자단계', '직원 수', '인수여부', 
+            '상장여부', '고객수(백만명)', '총 투자금(억원)', '연매출(억원)', 
+            'SNS 팔로우 수(백만명)', '기업가치(백억원)', '성공확률'
+        ]  # 모델 예측 수행
+    
+    # 결과를 JSON 형식으로 반환
+        result = {
+            "predictions": predictions.tolist(),
+            "count_0": int((predictions == 0).sum()),
+            "count_1": int((predictions == 1).sum())
+    }
+    
+        return jsonify(result)
+
     except Exception as e:
-        return jsonify({'error': str(e)}), 500  # 예외 처리
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=9999, debug=True)
+    app.run('127.0.0.1', port=9999, debug=False)
